77aa98c3081872546410556bba60065c4728bc03
Rasheeda Abbas <rasheedaabbas@gmail.com>
2020-06-19 18:40:16 +0500

    initial

---
+++ android/app/src/main/kotlin/com/rasheedaabbas/appquizzler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rasheedaabbas.appquizzler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: MainPage(),
+        ),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
